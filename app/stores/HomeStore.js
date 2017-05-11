851f5874984d45dc0774ef3f024ce6e96326a7c8
Franco Sirena <fisrena@gmail.com>
2017-05-10 21:41:09 -0300

    Creating first api handlers

---
+++ app/stores/HomeStore.js
@@ -0,0 +1,23 @@
+import alt from '../alt';
+import HomeActions from '../actions/HomeActions';
+
+class HomeStore {
+  constructor() {
+    this.bindActions(HomeActions);
+    this.characters = [];
+  }
+
+  onGetTwoCharactersSuccess(data) {
+    this.characters = data;
+  }
+
+  onGetTwoCharactersFail(jqXhr) {
+    toastr.error(jqXhr.responseJSON && jqXhr.responseJSON.message || jqXhr.responseText || jqXhr.statusText);
+  }
+
+  onVoteFail(jqXhr) {
+    toastr.error(jqXhr.responseJSON && jqXhr.responseJSON.message || jqXhr.responseText || jqXhr.statusText);
+  }
+}
+
+export default alt.createStore(HomeStore);
